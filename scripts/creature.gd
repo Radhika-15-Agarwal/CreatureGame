d25c652595e41ea361eaea95f99d7279c185674e
Radhika-15-Agarwal <24ucs128@lnmiit.ac.in>
2026-06-12 15:59:21 +0530

    Add creature exploration state

--- scripts/creature.gd
+++ scripts/creature.gd
@@ -3,10 +3,29 @@ extends CharacterBody2D
 var xp := 0
 var level := 1
 
+var exploring := false
+
 func gain_xp(amount):
 	xp += amount
-	
+
 	if xp >= level * 10:
 		xp = 0
 		level += 1
 		print("Level Up! ", level)
+
+func explore():
+	if exploring:
+		print("Already exploring")
+		return
+
+	exploring = true
+
+	print("Creature left to explore")
+
+	await get_tree().create_timer(2.0).timeout
+
+	gain_xp(5)
+
+	exploring = false
+
+	print("Creature returned")
--- scripts/main.gd
+++ scripts/main.gd
@@ -3,5 +3,4 @@ extends Node2D
 @onready var creature = $Creature
 
 func _on_explore_button_pressed():
-	creature.gain_xp(5)
-	print("Creature explored!")
+	creature.explore()
